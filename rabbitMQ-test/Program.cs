127f3263253ad4d5ce8d62950d401640a055c1ac
Yauheni Tabakerau <Yauheni.Tabakerau@itechart-group.com>
2021-03-01 17:04:43 +0300

    init publisher and subscriber progects

---
+++ rabbitMQ-test/Program.cs
@@ -0,0 +1,49 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace publisher
+{
+	internal sealed class Program
+	{
+		static void Main(string[] args)
+		{
+			var publisher = new Publisher();
+			publisher.Send("hello");
+
+			Console.ReadKey();
+		}
+	}
+
+	public sealed class Publisher
+	{
+		public const string DEFAULT_QUEUE_NAME = "defaultQueue";
+
+		public void Send(string message)
+		{
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+
+			using (var connection = factory.CreateConnection())
+			{
+				using (var channel = connection.CreateModel())
+				{
+					channel.QueueDeclare(queue: Publisher.DEFAULT_QUEUE_NAME,
+							durable: default,
+							exclusive: default,
+							autoDelete: default,
+							arguments: null);
+
+					var body = Encoding.UTF8.GetBytes(message);
+
+					channel.BasicPublish(
+							exchange: string.Empty,
+							routingKey: Publisher.DEFAULT_QUEUE_NAME,
+							basicProperties: null,
+							body: body);
+
+					Console.WriteLine("pulling message to queue");
+				}
+			}
+		}
+	}
+}
---
+++ subscriber/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace subscriber
+{
+	internal sealed class Program
+	{
+		static void Main(string[] args)
+		{
+			var sub = new Subscriber();
+			sub.PopMessage();
+
+			Console.ReadKey();
+		}
+	}
+
+	internal sealed class Subscriber
+	{
+		public void PopMessage()
+		{
+			var factory = new ConnectionFactory() { HostName = "localhost" };
+
+			using (var connection = factory.CreateConnection())
+			{
+				using (var channel = connection.CreateModel())
+				{
+					channel.QueueDeclare(queue: publisher.Publisher.DEFAULT_QUEUE_NAME,
+							durable: default,
+							exclusive: default,
+							autoDelete: default,
+							arguments: null);
+
+					var listener = new EventingBasicConsumer(channel);
+					listener.Received += (model, eventInfo) =>
+					{
+						if (eventInfo?.Body.Length == default)
+						{
+							throw new InvalidOperationException("message has no body");
+						}
+
+						var body = Encoding.UTF8.GetString(eventInfo.Body.ToArray());
+						Console.WriteLine($"publisher says: {body}");
+					};
+					channel.BasicConsume(
+							queue: publisher.Publisher.DEFAULT_QUEUE_NAME,
+							autoAck: true,
+							consumer: listener
+							);
+				}
+			}
+		}
+	}
+}
